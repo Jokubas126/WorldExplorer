9bee60115331886163be3d3f79b30c4a899c57ef
Jokubas Serapinas <43211120+Jokubas126@users.noreply.github.com>
2020-02-25 18:27:22 +0100

    changed toolbar design to have 1px border when being white and put toolbar bart of the layout to the activity_main layout

--- app/src/main/java/com/example/worldexplorer/MainActivity.kt
+++ app/src/main/java/com/example/worldexplorer/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.worldexplorer
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
